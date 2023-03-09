ac5ddaee78d69f4b1de0eaaa3da862d8a3a2e7bb
gobanger <you@example.com>
2023-03-09 16:45:12 +0100

    New

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodolistComponent } from './todo-list/todo-list.component';
+
+registerLocaleData(localeFr, 'fr');
 
 @NgModule({
   declarations: [
     AppComponent,
+    TodolistComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,9 +1,61 @@
-import { Component } from '@angular/core'; 
+import { Component, OnInit } from '@angular/core'; 
+import { IHotel } from './todo-list';
  
 @Component({ 
- selector: 'app-todolist', 
- templateUrl: './todo-list.component.html', 
- styleUrls: ['./todo-list.component.css']
+  selector: 'app-todo-list', 
+  templateUrl : './todo-list.component.html',
+  styleUrls: ['./todo-list.component.css']
 })
-export class TodoListComponent { 
-} 
+export class TodolistComponent implements OnInit{ 
+
+  public listTitle = 'Ma Todo List';  
+
+  public test = "Coucou";
+
+  public hotels: IHotel[]  = [
+      {
+            "hotelId": 1,
+            "hotelName": "Buea sweet life",
+            "description": "Belle vue au bord de la mer",
+            "price": 230.5,
+            "imageUrl": "assets/img/hotel-room.jpg",
+            //"rating": 3.5
+      },
+      {
+            "hotelId": 2,
+            "hotelName": "Marakech",
+            "description": "Profitez de la vue sur les montagnes",
+            "price": 145.5,
+            "imageUrl": "assets/img/the-interior.jpg",
+            //"rating": 5
+      },
+      {
+            "hotelId": 3,
+            "hotelName": "Abudja new look palace",
+            "description": "Séjour complet avec service de voitures",
+            "price": 120.12,
+            "imageUrl": "assets/img/indoors.jpg",
+            //"rating": 4
+      },
+      {
+            "hotelId": 4,
+            "hotelName": "Cape town city",
+            "description": "Magnifique cadre pour votre séjour",
+            "price": 135.12,
+            "imageUrl": "assets/img/window.jpg",
+            //"rating": 2.5
+      }
+  ];
+
+  public showBadge!: boolean;
+
+  public hotelFilter = 'mot';
+
+  ngOnInit(): void {
+      console.log('mon cycle fonctionne')
+  }
+
+  public toogleIsNewBadge() : void{
+    this.showBadge = !this.showBadge
+  }
+}
