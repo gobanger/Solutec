4aa165a7078919f8b16ab01dbeff4d6c57b5909d
gobanger <you@example.com>
2023-04-12 17:02:36 +0200

    ron

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ListeDeroulanteComponent } from './liste-deroulante/liste-deroulante.component';
 import { TodolistComponent } from './todo-list/todo-list.component';
 
 const routes: Routes = [
-  {path : 'listeHotel', component : TodolistComponent}
+  {path : 'listeHotel', component : TodolistComponent},
+  {path : 'liste-deroulante', component : ListeDeroulanteComponent},
+  {path : 'home', component : HomeComponent}
 ];
 
 @NgModule({
